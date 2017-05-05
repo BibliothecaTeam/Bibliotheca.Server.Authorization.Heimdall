5ba7754fb1e3159b3187df1bfe9395785484b7c2
Marcin Czachurski <mczachurski@icloud.com>
2017-05-05 15:03:44 +0200

    Add information about version to health endpoint.

--- src/Bibliotheca.Server.Authorization.Heimdall.Api/Controllers/HealthController.cs
+++ src/Bibliotheca.Server.Authorization.Heimdall.Api/Controllers/HealthController.cs
@@ -21,7 +21,8 @@ namespace Bibliotheca.Server.Authorization.Heimdall.Api.Controllers
         [ProducesResponseType(200, Type = typeof(string))]
         public string Get()
         {
-            return "I'm alive and reachable";
+            var version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion;
+            return $"[Heimdall: {version}] I'm alive and reachable";
         }
     }
 }
