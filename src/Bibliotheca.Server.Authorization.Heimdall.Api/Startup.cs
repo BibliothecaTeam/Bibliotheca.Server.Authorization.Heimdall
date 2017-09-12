5f1e56b873f3db03c5a2c020befea8346644f53d
Marcin Czachurski <mczachurski@icloud.com>
2017-09-12 07:24:56 +0200

    Fix authentication.

--- src/Bibliotheca.Server.Authorization.Heimdall.Api/Startup.cs
+++ src/Bibliotheca.Server.Authorization.Heimdall.Api/Startup.cs
@@ -20,6 +20,7 @@ using System.IO;
 using Swashbuckle.AspNetCore.Swagger;
 using Neutrino.AspNetCore.Client;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace Bibliotheca.Server.Authorization.Heimdall.Api
 {
@@ -70,37 +71,26 @@ namespace Bibliotheca.Server.Authorization.Heimdall.Api
 
             services.AddMvc(config =>
             {
-                var policy = new AuthorizationPolicyBuilder()
-                    .AddAuthenticationSchemes(SecureTokenSchema.Name)
-                    .AddAuthenticationSchemes(UserTokenSchema.Name)
-                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                var policy = new AuthorizationPolicyBuilder(
+                    new[] { SecureTokenSchema.Name, UserTokenSchema.Name, JwtBearerDefaults.AuthenticationScheme })
                     .RequireAuthenticatedUser()
                     .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
             }).AddJsonOptions(options =>
             {
                 options.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
             });
 
-            services.AddSingleton<ISecureTokenOptions>(new SecureTokenOptions { SecureToken = Configuration["SecureToken"] });
-            services.AddScoped<ISecureTokenAuthenticationHandler, SecureTokenAuthenticationHandler>();
-
             services.AddScoped<IUserTokenConfiguration, UserTokenConfiguration>();
-            services.AddScoped<IUserTokenAuthenticationHandler, UserTokenAuthenticationHandler>();
 
             services.AddAuthentication(configure => {
-                configure.AddScheme(SecureTokenSchema.Name, builder => {
-                    builder.DisplayName = SecureTokenSchema.Description;
-                    builder.HandlerType = typeof(ISecureTokenAuthenticationHandler);
-                });
-                configure.AddScheme(UserTokenSchema.Name, builder => {
-                    builder.DisplayName = UserTokenSchema.Description;
-                    builder.HandlerType = typeof(IUserTokenAuthenticationHandler);
-                });
                 configure.DefaultScheme = SecureTokenSchema.Name;
             }).AddBearerAuthentication(options => {
                 options.Authority = Configuration["OAuthAuthority"];
                 options.Audience = Configuration["OAuthAudience"];
-            });
+            }).AddSecureToken(options => {
+                options.SecureToken = Configuration["SecureToken"];
+            }).AddUserToken(options => { });
 
             services.AddApiVersioning(options =>
             {
